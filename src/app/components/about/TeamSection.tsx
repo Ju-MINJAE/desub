bebdd75c555ca0ab394f927950159e13983f65fe
jiseon_an <ajsajs1031@gmail.com>
2025-01-23 19:16:09 +0900

    feat: aboutpage ~team Section

--- src/app/components/about/TeamSection.tsx
+++ src/app/components/about/TeamSection.tsx
@@ -1,49 +1,47 @@
-import { H1 } from '@/stories/Heading.stories';
 import { TeamMember } from '@/types/about';
 import Heading from '../ui/Heading';
 
 const TeamSection: React.FC = () => {
   const teamMembers: TeamMember[] = [
     {
-      image: '/images/team1.jpg',
+      image: '/images/jisun.png',
       role: 'business developer / designer',
-      name: 'Son Lee',
+      name: 'Sun Lee',
     },
     {
-      image: '/images/team2.jpg',
+      image: '/images/jinam.png',
       role: 'art director / product designer',
-      name: 'JY lee',
+      name: 'PP Lee',
     },
     {
-      image: '/images/team3.jpg',
-      role: '3d / motion designer',
-      name: 'Yun hee',
+      image: '/images/yun.png',
+      role: '2d/ 3d motion designer',
+      name: 'Yun heo',
     },
   ];
 
   return (
-    <section className="mt-[48.4rem] ml-[22.4rem]">
+    <section className="mt-[48.4rem] mx-[22.4rem]">
       <Heading tag="h1">These people will join you</Heading>
-      <p className="text-[6rem] mt-[2.5] font-bold">
+      <p className="text-[6rem] mt-[2.5rem] font-bold">
         이 사람들과
         <br />
         함께 일하실 거에요.
       </p>
-
-      <div className="grid grid-cols-3 gap-[2rem]">
-        {teamMembers.map((member, index) => (
-          <div key={index} className="flex flex-col items-center">
-            <div className="rounded-[2rem] overflow-hidden mb-[2rem]">
-              <img
-                src={member.image}
-                alt={member.name}
-                className="w-[30rem] h-[35rem] object-cover"
-              />
+      <div className="mr-[0.3rem]">
+        <div className="grid gird-cols-1 lg:grid-cols-3 gap-[2rem] mt-[9.3rem]">
+          {teamMembers.map((member, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <div className="w-[31.6rem] h-[46.7rem]">
+                <img src={member.image} alt={member.name} className="w-full h-full" />
+              </div>
+              <div className="border rounded-[34.5rem] w-[34.5rem] h-[11.9rem] mt-[2rem] flex flex-col items-center justify-center">
+                <p className="text-[1.5rem] italic mb-[1rem]">{member.role}</p>
+                <p className="text-[1.5rem] font-medium">{member.name}</p>
+              </div>
             </div>
-            <span className="text-[2rem] italic">{member.role}</span>
-            <span className="text-[2.4rem] font-medium">{member.name}</span>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
 
       <div className="mt-[15rem] space-y-[3rem]">
