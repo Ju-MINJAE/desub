8b7f12fbcf40c563f0dba6fc3e26a6a23d59a107
Ju-MINJAE <juminjae98@gmail.com>
2025-01-23 16:33:19 +0900

    refactor: apply font-size 65%

--- src/app/components/Feature/HeroText.tsx
+++ src/app/components/Feature/HeroText.tsx
@@ -1,10 +1,10 @@
 const HeroText = () => {
   return (
     <div className="text-center mt-[17rem]">
-      <div className="font-normal text-[2.5rem]">
+      <div className="font-normal text-[4rem]">
         no more member change <br />& mis-communications!
       </div>
-      <div className="font-bold text-[6.25rem] text-center mt-[1.1875rem]">
+      <div className="font-bold text-[12.5rem] text-center mt-[1.9rem]">
         designer subscription system
       </div>
     </div>
