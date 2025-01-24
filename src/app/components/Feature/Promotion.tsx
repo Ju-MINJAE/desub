96b1dc6d1cebae5e5b209bc64dc5b035230e7b97
Ju-MINJAE <juminjae98@gmail.com>
2025-01-24 12:06:30 +0900

    feat: Create Promotion

---
+++ src/app/components/Feature/Promotion.tsx
@@ -0,0 +1,15 @@
+const Promotion = () => {
+  return (
+    <div className="w-full h-[9.6rem] pt-[18rem]">
+      <div className="bg-[#F3F3F3] p-8 overflow-hidden">
+        <p className="text-black text-[2.5rem] whitespace-nowrap overflow-hidden">
+          early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10%
+          off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early
+          bird 10% off 🐦‍⬛
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Promotion;
