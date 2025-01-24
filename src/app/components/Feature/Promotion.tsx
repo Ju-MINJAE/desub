ba8ad2d1f7c16e69c6a8085211bf6718ba967599
Ju-MINJAE <juminjae98@gmail.com>
2025-01-25 02:39:46 +0900

    feat: Apply marquee animation

--- src/app/components/Feature/Promotion.tsx
+++ src/app/components/Feature/Promotion.tsx
@@ -1,12 +1,35 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { promotionText } from '@/constants/promotion';
+
 const Promotion = () => {
+  const marqueeVariants = {
+    animate: {
+      x: [-1000, 0],
+      transition: {
+        x: {
+          repeat: Infinity,
+          repeatType: 'loop',
+          duration: 30,
+          ease: 'linear',
+        },
+      },
+    },
+  };
+
+  const repeatedText = promotionText.repeat(20);
+
   return (
     <div className="w-full h-[9.6rem] pt-[18rem]">
       <div className="bg-[#F3F3F3] p-8 overflow-hidden">
-        <p className="text-black text-[2.5rem] whitespace-nowrap overflow-hidden">
-          early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10%
-          off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early bird 10% off 🐦‍⬛ early
-          bird 10% off 🐦‍⬛
-        </p>
+        <motion.div
+          className="whitespace-nowrap inline-block"
+          variants={marqueeVariants}
+          animate="animate"
+        >
+          <p className="text-black text-[2.5rem] inline-block">{repeatedText}</p>
+        </motion.div>
       </div>
     </div>
   );
