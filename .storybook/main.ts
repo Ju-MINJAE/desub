f649faeaf86aa47a395154a4c82884b224401fdf
Ju-MINJAE <juminjae98@gmail.com>
2025-01-24 14:16:24 +0900

    chore: Setting staticDirs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,11 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
+  staticDirs: ['../public'],
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
   webpackFinal: async config => {
-    config.resolve = config.resolve || {}; // 기본값 설정
+    config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, '../src'),
