08ee96d669a1376c39b6baaad039c554900ea75d
hcartist <hcartist569@gmail.com>
2025-01-26 10:36:48 -0800

    feat: pricingText 모션 작업

--- src/app/components/Service/PricingProcess.tsx
+++ src/app/components/Service/PricingProcess.tsx
@@ -3,13 +3,40 @@
 import Heading from '@/app/components/ui/Heading';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import { useState, useEffect } from 'react';
 
 const PricingProcess = () => {
+  const [pricingText] = useState([
+    'design leader',
+    'UIUX designer',
+    'brand designer',
+    'product designer',
+  ]);
+  const [pricingTextCount, setPricingTextCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPricingTextCount(preIndex => (preIndex + 1) % 4);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="mt-[45.274rem] mb-[30.793rem] flex flex-col gap-[2.947rem] translate-x-[37rem]">
+    <div className="mt-[45.274rem] mb-[30.793rem] w-[80rem] flex flex-col translate-x-[37rem]">
       <Image src="/icons/ellipses.svg" alt="" width={318} height={99.572} />
       <Heading tag="h1">
-        design leader 가
+        <motion.div
+          className="mb-[6rem] inline"
+          initial={{ y: 50, opacity: 1 }}
+          animate={{ opacity: 0 }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+          }}
+        >
+          {pricingText[pricingTextCount]}
+        </motion.div>
+        가
         <br />
         필요하세요?
       </Heading>
@@ -20,14 +47,6 @@ const PricingProcess = () => {
         height={1}
         className="rotate-180 mt-[7.153rem]"
       />
-
-      <motion.div
-        initial={{ y: 50, opacity: 0 }} // 애니메이션 시작 시 위치와 투명도 설정
-        animate={{ y: 0, opacity: 1 }} // 애니메이션 끝 시 위치와 투명도 설정
-        transition={{ duration: 0.6 }} // 애니메이션 지속 시간 설정
-      >
-        <h1>휙휙! 위로 넘기는 텍스트</h1>
-      </motion.div>
     </div>
   );
 };
