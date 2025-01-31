1058d80d1fef984a52fe7c7b17adb76eab269ade
yoonhyeonjeong <h_j111@naver.com>
2025-01-31 14:51:36 +0900

    feat 회원가입 페이지 작업중

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Heading from '../components/ui/Heading';
 import { Button } from '../components/ui/Button';
 import { Input } from '../components/ui/Input';
 import TextButton from '@/app/components/ui/TextButton';
+import { formatTime } from '@/utils/time';
+import { formatPhoneNumber } from '@/utils/phone';
 
 export default function SignUp() {
+  const [isPhoneAuthDisabled, setIsPhoneAuthDisabled] = useState(false);
+  const [timeLeft, setTimeLeft] = useState<number | null>(null);
+  const [phone, setPhone] = useState('');
   const [checkboxes, setCheckboxes] = useState({
     all: false,
     terms: false,
@@ -34,7 +39,29 @@ export default function SignUp() {
       });
     }
   };
+  const handlePhoneAuthClick = () => {
+    setIsPhoneAuthDisabled(true);
+    setTimeLeft(239);
 
+    setTimeout(() => {
+      setIsPhoneAuthDisabled(false);
+      setTimeLeft(null);
+    }, 239000);
+  };
+
+  useEffect(() => {
+    if (timeLeft === null) return;
+
+    const timer = setInterval(() => {
+      setTimeLeft(prev => (prev !== null && prev > 0 ? prev - 1 : null));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(formatPhoneNumber(e.target.value));
+  };
   return (
     <div className="container mx-auto px-4 flex justify-center">
       <div className="w-full max-w-[98rem] flex flex-col items-center">
@@ -89,11 +116,41 @@ export default function SignUp() {
             <label htmlFor="phone" className="text-[3rem]">
               phone
             </label>
-            <Input id="phone" type="tel" placeholder="010-1234-5678" status="default" />
-            <Button variant="outline" className="!w-[14rem] h-[5rem] text-[2rem]">
+            <Input
+              id="phone"
+              type="tel"
+              placeholder="010-1234-5678"
+              value={phone}
+              onChange={handlePhoneChange}
+              status="default"
+            />
+            <Button
+              variant="outline"
+              size="small"
+              className="!w-[14rem] h-[5rem] text-[2rem]"
+              disabled={isPhoneAuthDisabled}
+              onClick={handlePhoneAuthClick}
+            >
               휴대폰 인증
             </Button>
           </div>
+
+          {isPhoneAuthDisabled && (
+            <div className="pl-[32rem] !mt-[3rem] grid grid-cols-[54rem_14rem] gap-x-8">
+              <Input
+                id="phone_auth"
+                type="number"
+                status="default"
+                placeholder="인증번호 입력"
+                helperText={`인증번호를 입력해주세요. ${formatTime(timeLeft)}`}
+                className="border p-[2rem] h-auto"
+                autoFocus
+              />
+              <Button variant="outline" className="!w-[14rem] h-[5rem] text-[2rem] mt-[2.2rem]">
+                인증 확인
+              </Button>
+            </div>
+          )}
 
           <div className="flex flex-col gap-10">
             <label className="flex items-center space-x-[2.3rem]">
