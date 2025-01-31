2c4d2a72428d62d0ebf34e8c5f639a47195b7bfa
Ju-MINJAE <juminjae98@gmail.com>
2025-01-31 10:32:42 +0900

    feat: In Progress signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,24 +1,53 @@
 'use client';
 
+import { useState } from 'react';
 import Heading from '../components/ui/Heading';
 import { Button } from '../components/ui/Button';
 import { Input } from '../components/ui/Input';
 
 export default function SignUp() {
+  const [checkboxes, setCheckboxes] = useState({
+    all: false,
+    terms: false,
+    privacy: false,
+    marketing: false,
+  });
+
+  const handleCheckboxChange = (name: keyof typeof checkboxes) => {
+    if (name === 'all') {
+      const newValue = !checkboxes.all;
+      setCheckboxes({
+        all: newValue,
+        terms: newValue,
+        privacy: newValue,
+        marketing: newValue,
+      });
+    } else {
+      const newCheckboxes = {
+        ...checkboxes,
+        [name]: !checkboxes[name],
+      };
+      setCheckboxes({
+        ...newCheckboxes,
+        all: newCheckboxes.terms && newCheckboxes.privacy && newCheckboxes.marketing,
+      });
+    }
+  };
+
   return (
-    <div className="mx-auto w-[80%] px-4">
-      <div className="flex flex-col items-center">
+    <div className="container mx-auto px-4 flex justify-center">
+      <div className="w-full max-w-[98rem] flex flex-col items-center">
         <Heading tag="h1" className="mt-10 mb-16">
           join
         </Heading>
 
-        <form className="w-full space-y-[10rem]">
+        <form className="w-full space-y-[8rem]">
           <div className="grid grid-cols-[30rem_54rem_14rem] gap-x-8 items-center">
             <label htmlFor="email" className="text-[3rem]">
               e-mail address
             </label>
             <Input id="email" type="email" placeholder="e-mail address" status="default" />
-            <Button variant="outline" className="w-[14rem] h-[5rem] text-[2rem]">
+            <Button variant="outline" className="!w-[14rem] h-[5rem] text-[2rem]">
               가입여부 확인
             </Button>
           </div>
@@ -60,9 +89,56 @@ export default function SignUp() {
               phone
             </label>
             <Input id="phone" type="tel" placeholder="010-1234-5678" status="default" />
-            <Button variant="outline" className="w-[14rem] h-[5rem] text-[2rem]">
+            <Button variant="outline" className="!w-[14rem] h-[5rem] text-[2rem]">
               휴대폰 인증
             </Button>
+          </div>
+
+          <div className="flex flex-col gap-10">
+            <label className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                className="peer hidden"
+                checked={checkboxes.all}
+                onChange={() => handleCheckboxChange('all')}
+              />
+              <span className="w-14 h-14 border-2 border-black rounded-sm peer-checked:bg-primary peer-checked:border-black"></span>
+              <span className="font-bold text-[3rem]">전체 동의</span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                className="peer hidden"
+                checked={checkboxes.terms}
+                onChange={() => handleCheckboxChange('terms')}
+              />
+              <span className="w-14 h-14 border-2 border-black rounded-sm peer-checked:bg-primary peer-checked:border-black"></span>
+              <span className="text-[3rem]">
+                <u className="font-bold">이용약관</u>에 동의합니다.(필수)
+              </span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                className="peer hidden"
+                checked={checkboxes.privacy}
+                onChange={() => handleCheckboxChange('privacy')}
+              />
+              <span className="w-14 h-14 border-2 border-black rounded-sm peer-checked:bg-primary peer-checked:border-black"></span>
+              <span className="text-[3rem]">
+                <u className="font-bold">개인정보처리방침</u>에 동의합니다.(필수)
+              </span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                className="peer hidden"
+                checked={checkboxes.marketing}
+                onChange={() => handleCheckboxChange('marketing')}
+              />
+              <span className="w-14 h-14 border-2 border-black rounded-sm peer-checked:bg-primary peer-checked:border-black"></span>
+              <span className="text-[3rem]">마케팅 수신에 동의합니다.(선택)</span>
+            </label>
           </div>
 
           <div className="grid grid-cols-[20rem_auto] gap-x-8 items-start mt-16">
