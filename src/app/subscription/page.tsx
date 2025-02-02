83d25c8e1b03b69cfe792bf2fb2458ca327c395f
hanhyochan <hcartist569@gmail.com>
2025-02-02 19:20:38 +0900

    feat: subcription 작업중

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -10,14 +10,15 @@ const Subscription = () => {
     <div className="h-full">
       <div className="pt-[4.7rem] px-[4.7rem] flex justify-between">
         <BackButton text="my subscription" />
-        <Button className="w-[11.9rem] h-[3.3rem]">리뷰 작성하기</Button>
+        <Button className="w-[11.9rem] h-[3.3rem] text-[1.5rem]" size="small" variant="outline">
+          리뷰 작성하기
+        </Button>
       </div>
 
       <div className="grid grid-cols-2 h-[71.2rem] mt-[2.9rem] px-[5.8rem]">
         <div className="flex flex-col gap-[9.9rem] justify-center border-r">
           <div className="mt-[5.5rem] flex flex-col items-center">
             <div className="w-[19.8rem] h-[19.8rem] bg-gray rounded-[100rem]"></div>
-
             <div className="mt-[2rem]">
               <p className="text-[5rem] font-extrabold italic">wassup</p>
               <div>
@@ -27,15 +28,16 @@ const Subscription = () => {
 
             <div className="flex gap-[2.7rem] mt-[5rem]">
               <Button
-                className="w-[20.9rem] h-[6rem] border border-black font-bold"
-                type="button"
+                className="w-[20.9rem] h-[6rem] border border-black font-bold text-2xl"
+                size="small"
                 variant="green"
               >
                 작업 요청하기
               </Button>
+
               <Button
-                className="w-[20.9rem] h-[6rem] border border-black font-extrabold"
-                type="button"
+                className="w-[20.9rem] h-[6rem] border border-black font-bold text-2xl"
+                size="small"
                 variant="outline"
               >
                 Workspace
@@ -49,6 +51,27 @@ const Subscription = () => {
 
           <Link href="/" className="font-extrabold text-[1.5rem] text-neutral-500">
             logout
+          </Link>
+        </div>
+
+        <div className="flex flex-col pl-[5.9rem] justify-center">
+          <div className="flex justify-between mt-[0.9rem]">
+            <p>Status</p>
+            <button>구독현황 변경 및 결제이력</button>
+          </div>
+          <div className="flex w-[40.1rem] justify-between">
+            <p className="text-[5rem] font-bold hover:underline hover:decoration-2">구독중</p>
+            <Button
+              className="w-[14.7rem] h-[6rem] font-bold text-2xl"
+              size="small"
+              variant="outline"
+            >
+              일시정지
+            </Button>
+          </div>
+          <p className="text-[1.8rem] font-extrabold mt-[1.8rem]">~2025.06.12 / D-593일 남음</p>
+          <Link href="/" className="text-[1.8rem] mt-[9.3rem] underline">
+            구독취소
           </Link>
         </div>
       </div>
