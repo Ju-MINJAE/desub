e3511a089dc37ab64727fc4fe410b96e33196a74
yoonhyeonjeong <h_j111@naver.com>
2025-02-06 17:51:52 +0900

    feat pricing page 추가 및 수정

---
+++ src/app/pricing/terms/page.tsx
@@ -0,0 +1,14 @@
+import Heading from '@/app/components/ui/Heading';
+
+export default function Terms() {
+  return (
+    <div className="container mx-auto px-4 flex justify-center">
+      <div className="w-full max-w-[98rem] flex flex-col items-center">
+        <Heading tag="h1" className="mt-[9rem] mb-[13rem]">
+          일시정지/구독취소/환불 규정
+        </Heading>
+        <div>규정 내용</div>
+      </div>
+    </div>
+  );
+}
