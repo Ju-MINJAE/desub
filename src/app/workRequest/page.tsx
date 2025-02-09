621fe8ef080e59dd8bf328217296f4c0933c7e64
hcartist <hcartist569@gmail.com>
2025-02-09 07:27:36 -0800

    feat: 업무별 리스트 방식 변경

--- src/app/workRequest/page.tsx
+++ src/app/workRequest/page.tsx
@@ -13,35 +13,52 @@ import CharacterForm from '../components/workRequest/CharacterForm';
 import EditingForm from '../components/workRequest/EditingForm';
 import MotionGraphicForm from '../components/workRequest/MotionGraphicForm';
 import Graphic3dForm from '../components/workRequest/Graphic3dForm';
+import Image from 'next/image';
 
 const WorkRequest: React.FC = () => {
-  const [selectedForm, setSelectedForm] = useState('logo');
+  const [selectedForm, setSelectedForm] = useState('업무 종류를 선택하세요.');
+  const [showWorkFormList, setShowWorkFormList] = useState(false);
 
-  const handleWorkSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedForm(e.target.value);
+  const handleWorkSelect = (item: string) => {
+    setSelectedForm(item);
+    setShowWorkFormList(false);
   };
+
+  const workList = [
+    '로고(logo)',
+    '웹사이트(website)',
+    '인쇄물(printing)',
+    '제안서(proposal)',
+    'SNS콘텐츠(sns contents)',
+    '서비스 기획(UI/UX)',
+    '캐릭터디자인(character)',
+    '영상편집(editing)',
+    'motio모션그래픽(motion graphic)',
+    '쓰리디(3D graphic)',
+  ];
   console.log(selectedForm);
+
   const handleFormSelect = () => {
     switch (selectedForm) {
-      case 'logo':
+      case '로고(logo)':
         return <LogoForm />;
-      case 'website':
+      case '웹사이트(website)':
         return <WebsiteForm />;
-      case 'printing':
+      case '인쇄물(printing)':
         return <PrintingForm />;
-      case 'proposal':
+      case '제안서(proposal)':
         return <ProposalForm />;
-      case 'snsContents':
+      case 'SNS콘텐츠(sns contents)':
         return <SnsContentsForm />;
-      case 'UI/UX':
+      case '서비스 기획(UI/UX)':
         return <UIUXForm />;
-      case 'character':
+      case '캐릭터디자인(character)':
         return <CharacterForm />;
-      case 'edting':
+      case '영상편집(editing)':
         return <EditingForm />;
-      case 'motionGraphic':
+      case 'motio모션그래픽(motion graphic)':
         return <MotionGraphicForm />;
-      case '3DGraphic':
+      case '쓰리디(3D graphic)':
         return <Graphic3dForm />;
       default:
         return <LogoForm />;
@@ -53,26 +70,34 @@ const WorkRequest: React.FC = () => {
       <BackButton className="pt-[4.7rem] px-[4.7rem]" text="작업 요청하기" />
       <div className="flex flex-col items-center pt-[9.2rem] gap-[6rem] px-[13.1rem]">
         <p className="text-[5rem] font-bold">작업요청하기</p>
-        <select
-          id="WorkSelector"
-          onChange={handleWorkSelect}
-          className="w-[43.7rem] h-[4.7rem] p-[0.8rem]"
-          defaultValue="text"
-        >
-          <option value="text" disabled>
-            업무 종류를 선택하세요
-          </option>
-          <option value="logo">로고(logo)</option>
-          <option value="website">웹사이트(website)</option>
-          <option value="printing">인쇄물(printing)</option>
-          <option value="proposal">제안서(proposal)</option>
-          <option value="snsContents">콘텐츠(sns contents)</option>
-          <option value="UI/UX">서비스 기획(UI/UX)</option>
-          <option value="character">캐릭터디자인(character)</option>
-          <option value="edting">영상편집(edting)</option>
-          <option value="motionGraphic">모션그래픽(motion graphic)</option>
-          <option value="3DGraphic">쓰리디(3D graphic)</option>
-        </select>
+        <div className="flex flex-col w-[43.7rem] text-[1.6rem] relative">
+          <div
+            className="flex w-full h-[4.7rem] justify-between  px-[1.7rem] py-[1.2rem] cursor-pointer"
+            onClick={() => setShowWorkFormList(prev => !prev)}
+          >
+            <p>{selectedForm}</p>
+            <Image
+              src="/icons/caret.svg"
+              className={showWorkFormList ? '' : 'rotate-180'}
+              alt="caret"
+              width={24}
+              height={24}
+            />
+          </div>
+          {showWorkFormList && (
+            <div className="border absolute top-full">
+              {workList.map((item, index) => (
+                <div
+                  key={index}
+                  className="bg-white w-[43.7rem] h-[4.7rem] px-[1.7rem] py-[1.2rem] cursor-pointer hover:bg-[#F9F9F9] active:bg-[#F9F9F9]"
+                  onClick={() => handleWorkSelect(item)}
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
         <div className="w-full bg-lightgray px-[6rem] py-[3rem] flex">{handleFormSelect()}</div>
       </div>
     </div>
