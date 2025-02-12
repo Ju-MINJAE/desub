4c0808af2877e0feb81a2c986f946b92a02de1e9
Ju-MINJAE <juminjae98@gmail.com>
2025-02-12 15:43:53 +0900

    feat: Add Home Link on Footer Image

--- src/app/components/ui/Footer.tsx
+++ src/app/components/ui/Footer.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className="w-full px-[13rem] py-[10rem]">
-      <Image
-        src="/images/desub_logo.png"
-        alt="Footer"
-        width={1920}
-        height={100}
-        className="w-full h-auto"
-      />
+      <Link href="/">
+        <Image
+          src="/images/desub_logo.png"
+          alt="Footer"
+          width={1920}
+          height={100}
+          className="w-full h-auto"
+        />
+      </Link>
       <p className="text-[2rem] mt-[5rem]">copyright(c) 2025. desub.</p>
     </footer>
   );
