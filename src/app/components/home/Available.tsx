c750f6b79b134a2521b30cbe4e48ce1650c6d230
Ju-MINJAE <juminjae98@gmail.com>
2025-02-13 15:09:45 +0900

    design: Responsive Design

--- src/app/components/home/Available.tsx
+++ src/app/components/home/Available.tsx
@@ -3,26 +3,55 @@
 import { badges } from '@/constants/badges';
 import Badge from '../ui/Badge';
 import { motion, useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Available = () => {
   const ref = useRef(null);
   const containerRef = useRef(null);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['start end', 'end start'],
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ['5%', '-10%']);
+  const x = useTransform(scrollYProgress, [0, 1], isMobile ? ['2%', '-5%'] : ['5%', '-10%']);
 
   return (
-    <div ref={ref} className="w-full px-[5rem] mt-20 overflow-hidden">
-      <h2 className="font-normal text-[4rem] ml-[13.5rem] block mb-20">We available for</h2>
-      <motion.div ref={containerRef} style={{ x }} className="flex gap-[3rem] w-max">
-        {badges.map(badge => (
-          <Badge key={badge.id} label={badge.label} variant={badge.variant} />
-        ))}
-      </motion.div>
+    <div ref={ref} className="w-full mt-20 px-[5rem] overflow-hidden">
+      <h2 className="ml-[4rem] md:ml-[13.5rem] mb-20 font-normal text-[1.8rem] md:text-[4rem] block">
+        We available for
+      </h2>
+
+      <div className="px-[2rem] md:px-[5rem]">
+        <motion.div
+          ref={containerRef}
+          style={{ x: isMobile ? 0 : x }}
+          className={`
+            flex gap-[1.5rem] md:gap-[3rem] 
+            ${isMobile ? 'overflow-x-auto scrollbar-hide' : 'w-max'}
+          `}
+        >
+          {badges.map(badge => (
+            <Badge
+              key={badge.id}
+              label={badge.label}
+              variant={badge.variant}
+              className="shrink-0"
+            />
+          ))}
+        </motion.div>
+      </div>
     </div>
   );
 };
--- src/app/components/home/FAQ_Accordion.tsx
+++ src/app/components/home/FAQ_Accordion.tsx
@@ -17,8 +17,10 @@ const FAQAccordion = () => {
   };
 
   return (
-    <div className="flex flex-col pt-[22.5rem]">
-      <h2 className="font-normal text-[4rem] ml-[13.5rem] block mb-20">FAQ</h2>
+    <div className="flex flex-col pt-[10rem] md:pt-[22.5rem]">
+      <h2 className="ml-[4rem] md:ml-[13.5rem] mb-20 font-normal text-[1.8rem] md:text-[4rem] block">
+        FAQ
+      </h2>
       <div className="w-full max-w-[75%] mx-auto">
         <div className="w-full h-[1px] bg-black"></div>
         <div>
@@ -26,18 +28,19 @@ const FAQAccordion = () => {
             <div key={index} className="border-b border-black">
               <button
                 onClick={() => toggleAccordion(index)}
-                className="flex w-full items-center justify-between py-4"
+                className="flex w-full items-center justify-between py-1 md:py-4"
               >
-                <span className="text-[3rem]">{item.question}</span>
-                <Image
-                  width={64}
-                  height={64}
-                  alt="chevron-down"
-                  src="/icons/chevron-down.svg"
-                  className={`transition-transform duration-200 ${
-                    openIndex === index ? 'rotate-180' : ''
-                  }`}
-                />
+                <span className="text-[1.6rem] md:text-[3rem]">{item.question}</span>
+                <div className="relative w-[3rem] h-[3rem] md:w-[6.4rem] md:h-[6.4rem]">
+                  <Image
+                    fill
+                    alt="accordion_open_close"
+                    src="/icons/chevron-down.svg"
+                    className={`object-contain transition-transform duration-200 ${
+                      openIndex === index ? 'rotate-180' : ''
+                    }`}
+                  />
+                </div>
               </button>
               <div
                 ref={el => {
@@ -48,7 +51,9 @@ const FAQAccordion = () => {
                 }`}
               >
                 <div className="overflow-hidden">
-                  <p className="pt-[6rem] pb-6 text-[2.5rem] whitespace-pre-line">{item.answer}</p>
+                  <p className="pt-[1.1rem] md:pt-[6rem] pd-[1.1rem] md:pb-6 text-[1.4rem] md:text-[2.5rem] whitespace-pre-line">
+                    {item.answer}
+                  </p>
                 </div>
               </div>
             </div>
