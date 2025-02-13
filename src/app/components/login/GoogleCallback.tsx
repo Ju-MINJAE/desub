22ee3eb7dbaf53fd4f054083bc1495e5838f3896
yoonhyeonjeong <h_j111@naver.com>
2025-02-13 10:19:27 +0900

    feat 구글 로그인 작업중

--- src/app/components/login/GoogleCallback.tsx
+++ src/app/components/login/GoogleCallback.tsx
@@ -1,31 +1,51 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { fetchGooglePost } from '@/api/auth';
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const GoogleCallback = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const code = searchParams.get('code') || '';
+  const code = searchParams.get('code');
+
+  const isCalled = useRef(false); // API 중복 호출 방지용 useRef 추가
 
   useEffect(() => {
-    if (!code) return;
-    const processGoogleLogin = async () => {
-      console.log('코드', code);
+    if (!code || !API_BASE_URL || isCalled.current) return;
 
+    isCalled.current = true; // 첫 번째 호출 이후에는 다시 실행되지 않도록 설정
+
+    const fetchGoogleLogin = async () => {
       try {
-        const data = await fetchGooglePost(code);
+        const response = await fetch(`${API_BASE_URL}/auth/google/login/?env=frontend_local`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ code }),
+        });
+
+        const data = await response.json();
+        console.log('✅ 로그인 응답 데이터:', data);
+
+        if (data.phone) {
+          router.push('/signup');
+        } else {
+          router.push('/');
+        }
       } catch (error) {
-        console.error('🚨 Google 로그인 처리 중 오류:', error);
+        console.error('🚨 구글 로그인 오류:', error);
+        alert('로그인 요청 중 문제가 발생했습니다. 다시 시도해주세요.');
         router.push('/login');
       }
     };
 
-    processGoogleLogin();
-  }, []);
+    fetchGoogleLogin();
+  }, [code, router]);
 
-  return <div>처리중...</div>;
+  return <></>;
 };
 
 export default GoogleCallback;
