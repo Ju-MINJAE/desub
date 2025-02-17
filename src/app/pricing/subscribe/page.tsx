772ac47cecc3b75acb4635fb6a2b129cafd493fc
hcartist <hcartist569@gmail.com>
2025-02-17 01:37:31 -0800

    feat: 결제 구현중

--- src/app/pricing/subscribe/page.tsx
+++ src/app/pricing/subscribe/page.tsx
@@ -7,7 +7,9 @@ import { Button } from '@/app/components/ui/Button';
 import { priceText } from '@/constants/price';
 import { useUserDataFetch } from '@/hooks/useUserDataFetch';
 import { createBillingKey } from '@/app/actions/paymentAction';
+import { Currency } from 'node_modules/@portone/browser-sdk/dist/v2/entity';
 const PORTONE_API_SECRET = process.env.NEXT_PUBLIC_PORTONE_API_SECRET;
+import { loadPaymentUI } from '@portone/browser-sdk/v2';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 import * as PortOne from '@portone/browser-sdk/v2';
@@ -34,57 +36,58 @@ const Subscribe = () => {
         issueId: `ISSUE${Date.now()}`,
         customer: {
           fullName: userData?.name,
-          // fullName: '한효찬',
         },
       });
 
       const paymentData = await createBillingKey(issueResponse?.billingKey as string);
+
       const billing_key = paymentData?.billing_key.billing_key;
       const paymentId = `${Date.now()}`;
 
-      const paymentResponse = await fetch(
-        `https://api.portone.io/payments/${encodeURIComponent(paymentId)}/billing-key`,
-        {
-          method: 'POST',
-          headers: {
-            Authorization: `PortOne ${PORTONE_API_SECRET}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            billingKey: billing_key,
-            orderName: '월간 이용권 정기결제',
-            customer: {
-              fullName: userData?.name,
-            },
-            amount: {
-              total: paymentData?.planData[0].price,
-            },
-            currency: 'KRW',
-          }),
-        },
-      );
-
-      if (!paymentResponse.ok) {
-        const errorResponse = await paymentResponse.json();
-        throw new Error(`paymentResponse: ${JSON.stringify(errorResponse)}`);
-      }
-
-      // const paymentResponse = await PortOne.requestPayment({
-      //   storeId: 'store-c25c9523-5081-4aae-a882-ce7e52479c59',
-      //   channelKey: 'channel-key-8bc12c40-b958-4151-ae85-98c129a80099',
-      //   paymentId: paymentId,
-      //   orderName: paymentData?.planData[0].plan_name,
-      //   totalAmount: paymentData?.planData[0].price,
-      //   currency: 'CURRENCY_KRW',
-      //   payMethod: 'CARD',
-      //   billingKey: paymentData?.billing_key.billing_key,
-      //   extra: {
-      //     pg: 'kpn',
-      //     method: 'card',
-      //     name: 'KPN 정기결제',
-      //     showResult: true,
+      // const paymentResponse = await fetch(
+      //   `https://api.portone.io/payments/${encodeURIComponent(paymentId)}/billing-key`,
+      //   {
+      //     method: 'POST',
+      //     headers: {
+      //       Authorization: `PortOne ${PORTONE_API_SECRET}`,
+      //       'Content-Type': 'application/json',
+      //     },
+      //     body: JSON.stringify({
+      //       billingKey: billing_key,
+      //       orderName: '월간 이용권 정기결제',
+      //       customer: {
+      //         fullName: userData?.name,
+      //       },
+      //       amount: {
+      //         total: paymentData?.planData[0].price,
+      //       },
+      //       currency: 'KRW',
+      //     }),
       //   },
-      // });
+      // );
+      // if (!paymentResponse.ok) {
+      //   const errorResponse = await paymentResponse.json();
+      //   throw new Error(`paymentResponse: ${JSON.stringify(errorResponse)}`);
+      // }
+
+      const paymentResponse = await PortOne.requestPayment({
+        storeId: 'store-c25c9523-5081-4aae-a882-ce7e52479c59',
+        channelKey: 'channel-key-8bc12c40-b958-4151-ae85-98c129a80099',
+        paymentId: paymentId,
+        orderName: paymentData?.planData[0].plan_name,
+        totalAmount: paymentData?.planData[0].price,
+        currency: 'CURRENCY_KRW',
+        payMethod: 'CARD',
+        billingKey: billing_key,
+        extra: {
+          pg: 'kpn',
+          method: 'card',
+          name: 'KPN 정기결제',
+          showResult: true,
+        },
+      });
+
+      console.log(paymentResponse);
     } catch (error) {
       console.log(error);
     }
