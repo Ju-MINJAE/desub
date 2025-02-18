3b893ba530b9f4214a34936ab7c0bb50d64109d3
Ju-MINJAE <juminjae98@gmail.com>
2025-02-18 13:43:45 +0900

    feat: Implement authentication flow
    - Add middleware to handle user authentication
    - Redirect unauthenticated uesrs to login page
    - Prevent authenticated users from accessing login/signup pages
    - Use refresh token to determine authentucation status
    - Configure middleware matcher for relevant routes

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+export const middleware = (req: NextRequest) => {
+  const refreshToken = req.cookies.get('refresh_token')?.value;
+  // console.log('refreshToken: ', refreshToken);
+
+  const { pathname } = req.nextUrl;
+  const isAuthenticated = refreshToken && refreshToken.trim() !== '';
+
+  if (
+    !isAuthenticated &&
+    (pathname.startsWith('/myInfo') || pathname.startsWith('/subscription'))
+  ) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (isAuthenticated && (pathname.startsWith('/signup') || pathname.startsWith('/login'))) {
+    return NextResponse.redirect(new URL('/myInfo', req.url));
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ['/myInfo/:path*', '/subscription/:path*', '/signup/:path*', '/login/:path*'],
+};
