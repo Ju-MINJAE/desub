6ef4a93a4608ed4edbd45ae2b701e1bf6ce57c41
Ju-MINJAE <juminjae98@gmail.com>
2025-02-19 16:46:37 +0900

    feat: Connect getSubscriptionHistory api

--- src/api/subscription.ts
+++ src/api/subscription.ts
@@ -9,9 +9,7 @@ interface SubscriptionResponse {
 }
 
 export const statusSubscriptions = async (accessToken: string): Promise<SubscriptionResponse> => {
-  if (!accessToken) {
-    return { status: 'error', error: '인증 토큰이 없습니다.' };
-  }
+  if (!accessToken) return { status: 'error', error: '인증 토큰이 없습니다.' };
 
   try {
     const response = await fetch(`${API_BASE_URL}/api/subscriptions/`, {
@@ -43,5 +41,50 @@ export const statusSubscriptions = async (accessToken: string): Promise<Subscrip
       status: 'error',
       error: '구독 상태를 가져오는 데 실패했습니다.',
     };
+  }
+};
+
+export interface SubscriptionHistoryItem {
+  id: number;
+  change_date: string;
+  status: string;
+  sub: number;
+  user: string;
+  plan: number;
+}
+
+interface ApiResponse {
+  status: 'success' | 'error';
+  data?: SubscriptionHistoryItem[];
+  error?: string;
+}
+
+export const getSubscriptionHistory = async (accessToken: string): Promise<ApiResponse> => {
+  if (!accessToken) {
+    return { status: 'error', error: '인증 토큰이 없습니다.' };
+  }
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/subscriptions/history/`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      return { status: 'error', error: `HTTP error! status: ${response.status}` };
+    }
+
+    const data = await response.json();
+    return { status: 'success', data };
+  } catch (err) {
+    console.error(err);
+    if (err instanceof Error) {
+      return { status: 'error', error: err.message };
+    }
+    return { status: 'error', error: 'An unknown error occurred' };
   }
 };
