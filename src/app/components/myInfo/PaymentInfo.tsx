918b5993bf392003f1994a1363d6a2134b8c3616
Ju-MINJAE <juminjae98@gmail.com>
2025-02-20 15:14:23 +0900

    fix: useAppSelector

--- src/app/components/myInfo/PaymentInfo.tsx
+++ src/app/components/myInfo/PaymentInfo.tsx
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
-import { Button } from '../ui/Button';
-import { useUserDataFetch } from '@/hooks/useUserDataFetch';
 import { format } from 'date-fns';
+import { useAppSelector } from '@/hooks/redux/hooks';
+import { Button } from '../ui/Button';
 
 const PaymentInfo = () => {
-  const { userData, getUserData } = useUserDataFetch();
-
-  useEffect(() => {
-    getUserData();
-  }, []);
+  const userData = useAppSelector(state => state.userData);
 
   const cardInfo = userData?.subscription_info;
   console.log(cardInfo);
