3da2d56c8d4a5680f1a22360fc0b9b3c25d17573
yoonhyeonjeong <h_j111@naver.com>
2025-02-21 18:43:54 +0900

    feat 탈퇴모달 작업중

--- src/app/components/myInfo/PaymentInfo.tsx
+++ src/app/components/myInfo/PaymentInfo.tsx
@@ -1,10 +1,42 @@
 import { format } from 'date-fns';
 import { useAppSelector } from '@/hooks/redux/hooks';
-import { Button } from '../ui/Button';
+import { useState } from 'react';
+import { Button } from '@/app/components/ui/Button';
+import { Alert } from '@/app/components/ui/Alert';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { WithdrawalSchema, WithdrawalValue } from '@/app/auth/schemas/WithdrawalSchema';
 
 const PaymentInfo = () => {
+  const [isWithdrawalModalOpen, setIsWithdrawalModalOpen] = useState(false); // 탈퇴 모달 열림 여부
   const userData = useAppSelector(state => state.userData);
+  // 탈퇴 팝업
+  const handleOpenPopup = () => {
+    setIsWithdrawalModalOpen(true);
+  };
+  const handleClosePopup = () => {
+    setIsWithdrawalModalOpen(false);
+  };
 
+  // useform
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<WithdrawalValue>({
+    resolver: zodResolver(WithdrawalSchema),
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (data: WithdrawalValue) => {
+    try {
+      console.log('api 호출');
+    } catch (error) {
+      console.error('탈퇴사유 전송 실패', error);
+    }
+  };
+  // 결제정보
   const cardInfo = userData?.subscription_info;
   const formattedCardNum = cardInfo?.card_number || '등록된 카드가 없습니다.';
   const formattedCardName = cardInfo?.card_name || '';
@@ -16,6 +48,7 @@ const PaymentInfo = () => {
     : '-';
   const formattedEmail = cardInfo ? userData?.email || '결제 이메일 없음' : null;
 
+  const withdrawalReason = watch('reason') || '';
   return (
     <div className="flex flex-col gap-[5rem] w-full md:w-[57.4rem] ">
       <div className="flex items-center">
@@ -56,6 +89,51 @@ const PaymentInfo = () => {
         >
           결제정보 변경
         </Button>
+      )}
+      <div className="align-baseline pt-[14.3rem]">
+        <button
+          className="text-[1.6rem] leading-[2.4rem] font-medium underline"
+          onClick={handleOpenPopup}
+        >
+          탈퇴하기
+        </button>
+      </div>
+
+      {/* 탈퇴하기모달 */}
+      {isWithdrawalModalOpen && (
+        <Alert
+          buttonText="작성완료"
+          contents={
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="pb-[4rem] flex flex-col items-center gap-[2rem]"
+            >
+              <textarea
+                {...register('reason')}
+                className="w-full h-[20.7rem] border border-black p-[1rem]"
+                value={withdrawalReason}
+                placeholder="여기에 솔직한 후기를 작성해주세요."
+              ></textarea>
+              {errors.reason?.message && (
+                <div className=" w-full">
+                  <p className="text-red text-[1.6rem]">{errors.reason?.message}</p>
+                </div>
+              )}
+            </form>
+          }
+          title={
+            <>
+              구독기간 중 언제든 리뷰를 작성하실 수 있어요.
+              <br />
+              작성된 리뷰는 서비스 개선에 참고하겠습니다.
+            </>
+          }
+          size="normal"
+          variant="green"
+          onClose={handleClosePopup}
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-[60rem] min-h-[60.4rem]"
+        />
       )}
     </div>
   );
