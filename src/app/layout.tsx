3753ebfbc6fbf3741c7bd3b70466d21066882f21
Ju-MINJAE <juminjae98@gmail.com>
2025-02-24 16:25:07 +0900

    chore: add metadata configuration for improved social sharing and search visibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,15 @@ import Footer from './components/ui/Footer';
 import Header from './components/ui/Header';
 import StoreProvider from './StoreProvider';
 
-// export const metadata: Metadata = {
-//   title: 'desub',
-//   description: '디자인 월 구독 서비스',
-// };
+export const metadata: Metadata = {
+  title: 'desub',
+  description: '디자인 월 구독 서비스',
+  openGraph: {
+    title: 'desub',
+    description: '디자인 월 구독 서비스',
+    images: ['/images/desub_ thumbnail.png'],
+  },
+};
 
 export default function RootLayout({
   children,
