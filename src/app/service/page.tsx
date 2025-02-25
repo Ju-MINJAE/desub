f24444120c4012c4d401c24f8df54121f667e2ca
hcartist <hcartist569@gmail.com>
2025-02-25 01:54:51 -0800

    design: CSS 수정

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import Heading from '@/app/components/ui/Heading';
-import Specialists from '../components/service/Specialists';
-import PricingProcess from '../components/service/PricingProcess';
+import Specialists from '../components/Service/Specialists';
+import PricingProcess from '../components/Service/PricingProcess';
 import Image from 'next/image';
 import LoadingWrapper from '../components/ui/LoadingWrapper';
 import { motion } from 'framer-motion';
@@ -54,7 +54,7 @@ const Service = () => {
                 },
               },
             }}
-            className="font-extrabold text-[1.8rem] md:text-[4rem]"
+            className="font-bold text-[1.8rem] md:text-[4rem]"
           >
             이 분야 만큼은
             <br />
@@ -108,7 +108,7 @@ const Service = () => {
                 },
               },
             }}
-            className="text-[1.8rem] font-extrabold md:text-[3rem]"
+            className="text-[1.8rem] font-bold md:text-[3rem]"
           >
             업무 효율에 가장 최적화된 생산성 툴을 사용하고 있습니다.
           </motion.p>
