6326ef5221a012a0a3177d94ac5ce667030d3095
Ju-MINJAE <juminjae98@gmail.com>
2025-02-25 14:09:51 +0900

    chore: Remove Logout Button on Header

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -11,7 +11,6 @@ import MobileMenu from './MobileMenu';
 import { useAppSelector } from '@/hooks/redux/hooks';
 import type { RootState } from '@/store/store';
 import { useAppDispatch } from '@/hooks/redux/hooks';
-import { logoutUser } from '@/api/auth';
 import { getUserSession, clearUserSession } from '@/app/actions/serverAction';
 import { logout } from '@/store/authslice';
 import { clearUserData } from '@/store/userDataSlice';
@@ -36,22 +35,6 @@ const Header = () => {
     };
     checkSession();
   }, [dispatch]);
-
-  // 임시 로그아웃
-  const logoutSession = async () => {
-    const { accessToken, refreshToken } = await getUserSession();
-    if (accessToken && refreshToken) {
-      try {
-        await logoutUser(accessToken, refreshToken); // 로그아웃 api
-        alert('로그아웃 완료');
-      } catch (error) {
-        console.error('로그아웃 실패', error);
-      }
-    }
-    await clearUserSession(); // 토큰 삭제
-    dispatch(logout());
-    dispatch(clearUserData());
-  };
 
   if (
     pathname.startsWith('/login') ||
@@ -148,12 +131,6 @@ const Header = () => {
                 >
                   workspace
                 </Link>
-                <button
-                  className="ml-[2rem] lg:ml-[3.9rem] text-[1.6rem] lg:text-[2rem] hidden md:block"
-                  onClick={logoutSession}
-                >
-                  logout
-                </button>
               </>
             ) : (
               //  로그인 후 미구독
@@ -182,12 +159,6 @@ const Header = () => {
                   >
                     my subscription
                   </Link>
-                  <button
-                    className="ml-[2rem] lg:ml-[3.9rem] text-[1.6rem] lg:text-[2rem] hidden md:block"
-                    onClick={logoutSession}
-                  >
-                    logout
-                  </button>
                 </div>
               </div>
             )
