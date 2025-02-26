6c95eb5863c6eba3163b32442a400c0c0d29ce02
yoonhyeonjeong <h_j111@naver.com>
2025-02-26 11:53:31 +0900

    fix main 가로스크롤 hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ const Home = () => {
 
   return (
     <LoadingWrapper>
-      <div className="flex flex-col">
+      <div className="flex flex-col overflow-x-hidden">
         <BannerImage />
         <HeroText />
         <Features />
