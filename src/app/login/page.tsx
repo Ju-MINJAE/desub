8056f1418a7b34639be7ab7b70e88a1e857dc2d7
yoonhyeonjeong <h_j111@naver.com>
2025-02-26 12:13:54 +0900

    Fix login 반응형 디자인 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,7 +19,7 @@ const Login = () => {
     <LoadingWrapper>
       <div className="flex flex-col md:flex-row min-h-full md:min-h-screen">
         <div className="w-full md:w-1/2 md:order-2 mb-[8rem] md:mb-0">
-          <div className="relative w-full h-[22.3rem] md:h-full">
+          <div className="relative w-full h-[22.3rem] md:h-full hidden md:flex">
             <Image
               src="/images/login_intro.png"
               alt="desub_login"
@@ -31,7 +31,7 @@ const Login = () => {
           </div>
         </div>
 
-        <div className="w-full md:w-1/2 flex flex-col justify-center items-center text-center p-8 md:p-0 md:order-1">
+        <div className="w-full min-h-[100vh] md:min-h-auto md:w-1/2 flex flex-col justify-center items-center text-center p-8 md:p-0 md:order-1">
           <div>
             <Heading tag="h1" className="mb-[5rem] md:mb-[16.8rem]">
               login
